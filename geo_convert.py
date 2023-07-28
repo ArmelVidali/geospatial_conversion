4dbd2c05fcf19a7156d9d0c33946dce4cf249501
Armel Vidali <vidali.armel@gmail.com>
2023-07-28 11:24:40 +0200

    added radio button, confirmation popup

--- geo_convert.py
+++ geo_convert.py
@@ -2,24 +2,32 @@ import geopandas as gpd
 import os
 
 def import_data(path, output_format =False, target_crs = False):
-    gdf = gpd.read_file(path)
-    if target_crs : 
-        gdf = gdf.to_crs(target_crs)
-    path = path.replace("\\", "/")
-    file_name = path[path.rfind("/")+1:path.rfind(".")]
+    try : 
+        gdf = gpd.read_file(path)
+    
+        if target_crs : 
+            gdf = gdf.to_crs(target_crs)
+        path = path.replace("\\", "/")
+        file_name = path[path.rfind("/")+1:path.rfind(".")]
+            
+        output_path = path[:path.rfind("/")] + "/geoconvert_to_" + str(output_format) 
+    
         
-    output_path = path[:path.rfind("/")] + "/geoconvert_to_" + str(output_format) 
- 
-    if os.path.isdir(output_path):
-        if output_format == "csv" : 
-            gdf.to_csv(output_path + "/" + file_name + "." + output_format)
-        else : 
-            gdf.to_file(output_path + "/" + file_name + "." + output_format)
-    else:
-        os.mkdir(output_path) 
-        if output_format == "csv" : 
-            gdf.to_csv(output_path + "/" + file_name + "." + output_format)
-        else : 
-            gdf.to_file(output_path + "/" + file_name + "." + output_format)
+        if os.path.isdir(output_path):
+            if output_format == "csv" : 
+                gdf.to_csv(output_path + "/" + file_name + "." + output_format)
+            else : 
+                gdf.to_file(output_path + "/" + file_name + "." + output_format)
+        else:
+            os.mkdir(output_path) 
+            if output_format == "csv" : 
+                gdf.to_csv(output_path + "/" + file_name + "." + output_format)
+            else : 
+                gdf.to_file(output_path + "/" + file_name + "." + output_format)
+        return [1, output_path]
+    except :
+        return[0, ""]
+
+    
             
 
--- interface.py
+++ interface.py
@@ -5,14 +5,42 @@ from geo_convert import import_data
 def browse_file():
     file_path = filedialog.askopenfilenames()
     epsg = int_entry.get()
-    if epsg :
+    if epsg:
         pass
-    else :
+    else:
         epsg = False
     selected_format = option_var.get()
+    print(selected_format)
+    count = 0 
     
     for element in file_path:
-        import_data(element, selected_format, epsg)
+        success = import_data(element, selected_format, epsg)
+        if success[0]:
+            count +=1
+
+    if count : 
+        show_confirmation_popup(True, success[1], count)
+    else :
+        show_confirmation_popup(False)
+
+def show_confirmation_popup(success , path = "", count = 0):
+    if success : 
+        popup = tk.Toplevel(root)
+        popup.title("Success")
+        popup.geometry("380x150")
+
+        message_label = tk.Label(popup, text="Files successfully converted", font=("Arial", 12, "bold"), fg = "green")
+        message_label.pack(pady=20)
+        path_label = tk.Label(popup, text= str(count) + " file.s was converted to the " + path + " folder", font=("Arial", 10))
+        path_label.pack(pady=20)
+
+    else : 
+        popup = tk.Toplevel(root)
+        popup.title("Error")
+        popup.geometry("350x150")
+
+        message_label = tk.Label(popup, text="An error occured", font=("Arial", 12, "bold"), fg = "red")
+        message_label.pack(pady=20)
 
 def select_output_type():
     # Implement the logic to select the type of output file here
@@ -29,8 +57,8 @@ def stop_blinking_cursor(event):
     root.focus()  # Set focus to the root window to remove focus from the input field
 
 root = tk.Tk()
-root.title("Geogrphic file converter")
-root.geometry("600x600")
+root.title("Geographic File Converter")
+root.geometry("600x300")
 
 font_family = "Arial"
 font_size = 16
@@ -39,47 +67,41 @@ font_color = "red"
 
 custom_font = (font_family, font_size, font_weight)
 
-
-# Button to select the type of output file
-output_type_choice = tk.Label(root, text="Choose your ouput format", font=custom_font, fg="blue")
+# Title: Choose your output format
+output_type_choice = tk.Label(root, text="Choose your output format", font=custom_font, fg="blue")
 output_type_choice.pack()
 
-options = ["GeoJson", "Geopackage", "Shapefile","csv"]
+options = ["geojson", "gpkg", "shp", "csv"]
 
-option_var = tk.StringVar(root)
-option_var.set(options[0])  # Set the default selected option
+# Variable to store the selected option
+option_var = tk.StringVar()
+option_var.set(options[0])
 
-option_menu = tk.OptionMenu(root, option_var, *options)
-option_menu.pack()
+# Frame to hold the radio buttons and optional input field
+radio_frame = tk.Frame(root)
+radio_frame.pack()
 
-output_type_choice = tk.Label(root, text="Optionnal : Choose the output projection for conversion (type in the EPSG code, ex : 3857 for Pseudo-Mercator)", fg="gray")
+# Create the radio buttons in a horizontal row inside the frame
+for option in options:
+    tk.Radiobutton(radio_frame, text=option, variable=option_var, value=option).pack(side=tk.LEFT)
+
+# Title: Optional: Choose the output projection for conversion
+output_type_choice = tk.Label(root, text="Optional: Choose the output projection for conversion (type in the EPSG code, ex: 3857 for Pseudo-Mercator)", fg="gray")
 output_type_choice.pack()
+
+# Optional input field
 int_entry = tk.Entry(root, validate="key", validatecommand=(validate_int_input, '%d', '%P'), width=5)
 int_entry.pack()
-
-
 
 int_entry.bind("<Return>", stop_blinking_cursor)
 int_entry.bind("<FocusOut>", stop_blinking_cursor)
 
-options = ["GeoJson", "Geopackage"]
-
-option_var = tk.StringVar(root)
-option_var.set(options[0])  # Set the default selected option
-
-option_menu = tk.OptionMenu(root, option_var, *options)
-option_menu.pack()
-
-
-
 # Text
 text_label = tk.Label(root, text="Browse for files/folder")
-text_label.pack(pady=(20,0))
+text_label.pack(pady=(20, 0))
 
 # Button to browse file
 browse_button = tk.Button(root, text="Browse", command=browse_file)
 browse_button.pack(pady=10)
-
-
 
 root.mainloop()
